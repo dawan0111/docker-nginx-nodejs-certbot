4aa1b1d029e1ceb381b502f667bb131bca84edd2
KDW <42798237+dawan0111@users.noreply.github.com>
2022-07-28 17:28:09 +0900

    [ssl_installer] add pwd path

--- ssl_install.py
+++ ssl_install.py
@@ -1,8 +1,10 @@
+# -*- coding: utf-8 -*-
+
 import os
 
 if __name__ == "__main__":
   print("ssl installer")
-  email = input("Enter your email: ")
+  email = input('Enter your email: ')
   domains = []
   while True:
     domain = input("Enter your domain:")
@@ -10,15 +12,17 @@ if __name__ == "__main__":
       break
     domains.append("-d {0}".format(domain))
 
+  CWD = os.getcwd()
+
   try:
     os.system("""
       docker run -it --rm --name certbot \
-      -v './certbot/etc:/etc/letsencrypt/:rw' \
-      -v './certbot/var:/var/lib/letsencrypt/:rw' \
-      -v './certbot/www:/var/www/certbot/:rw' \
+      -v '/{2}/certbot/etc:/etc/letsencrypt/:rw' \
+      -v '/{2}/certbot/var:/var/lib/letsencrypt/:rw' \
+      -v '/{2}/certbot/www:/var/www/certbot/:rw' \
       certbot/certbot \
       certonly --webroot --webroot-path=/var/www/certbot --email {0} --agree-tos --no-eff-email --force-renewal {1}
-    """.format(email, " ".join(domains)))
+    """.format(email, " ".join(domains), CWD))
   except Exception as e:
     print(e)
   
